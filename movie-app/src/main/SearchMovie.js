2b48759033672ff8ca18b4cc5c1ace941feeae1e
ZachyDev <mosetizachary001@gmail.com>
2020-06-06 00:59:22 +0300

    fetched data from TMDB

--- movie-app/src/main/SearchMovie.js
+++ movie-app/src/main/SearchMovie.js
@@ -1,23 +1,23 @@
 import React, { Component } from 'react'
-
+import axios from 'axios';
 class SearchMovie extends Component {
     constructor(props) {
         super(props)
     
         this.state = {
-             name: '',
+             query: '',
              movieDb: [],
              hasError: ''
         }
     }
     fetchResult =  (e) => {
+        const { query } =  this.state;
         e.preventDefault();
-        const  query = "avengers"
-        fetch(`https://api.themoviedb.org/3/search/movie?api_key=7bf9c75abbb6e0954ea40e8d52ff1ba4&language=en-US&query=${query}&page=1&include_adult=false`)
+        axios.get(`https://api.themoviedb.org/3/search/movie?api_key=7bf9c75abbb6e0954ea40e8d52ff1ba4&language=en-US&query=${query}&page=1&include_adult=false`)
         .then(response => {
             let data = response.json();
             console.log(data);
-            this.setState({ movieDb: data})
+            this.setState({ movieDb: data.results})
         })
         .catch(error => {
             this.setState({ hasError: error})
@@ -25,11 +25,13 @@ class SearchMovie extends Component {
     }
     handleChange = (e) => {
         this.setState({
-            name: e.target.value
+            query: e.target.value
         })
     }
     render() {
-        const { name } = this.state;
+        const { query,movieDb,hasError } = this.state;
+        const movieList =  movieDb.map(movie => <div key = { movie.id }> <li>{ movie.original_title }</li></div>)
+
         return (
             <div>
                 <form onSubmit = { this.fetchResult }>
@@ -38,12 +40,13 @@ class SearchMovie extends Component {
                      type = "text"
                      name = "query" 
                      placeholder = "Search Movie"
-                     value = { name }
+                     value = { query }
                      onChange = { this.handleChange }
                      />
                      <button className = "button" type="submit">Search</button>
 
                 </form>
+                { movieDb }
             </div>
         )
     }
