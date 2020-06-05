dd7de566d3adc33266b2cf102948f942e7b84e23
ZachyDev <mosetizachary001@gmail.com>
2020-06-05 23:32:04 +0300

    added the movies component with form

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import SearchMovie from './main/SearchMovie';
 
 function App() {
   return (
     <div className="container">
         <h1 className ="title">Movies Cloud9</h1>
+        <SearchMovie />
     </div>
   );
 }
---
+++ movie-app/src/main/SearchMovie.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+
+class SearchMovie extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             name: ''
+        }
+    }
+    handleChange = (e) => {
+        this.setState({
+            name: e.target.value
+        })
+    }
+    render() {
+        const { name } = this.state;
+        return (
+            <div>
+                <form>
+                    <label forHtml="query" className="Label"></label>
+                    <input 
+                     type = "text"
+                     name = "query" 
+                     placeholder = "Search Movie"
+                     value = { name }
+                     onChange = { this.handleChange }
+                     />
+                     <button className = "button" type="submit">Search</button>
+
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SearchMovie
